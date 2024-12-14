f98a7b962d2a45a4aeae7a191ef4547ac4ee6449
Suraj Bhatt <sbhatt@aptean.com>
2024-12-14 20:15:56 +0530

    create a basic example of vectorstore and embeddings

--- 4.py
+++ 4.py
@@ -1,61 +1,27 @@
-"""Create and invoke a chain (LCEL syntax)"""
+"""Create a vector store and perform similarity search"""
 
 from dotenv import load_dotenv
 from colorama import Fore
 import os
 
-from langchain_openai import OpenAI
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_openai import OpenAI, OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import FAISS
 
 
 load_dotenv()
 
-llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
 prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}?Only give joke.")
+
+llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 output_parser = StrOutputParser()
 
+# create a vector store and embeddings
+vectorstore = FAISS.from_texts(["harrison worked on kensho"], embedding=OpenAIEmbeddings())
 
-
-def generate(topic):
-    """ generate text based on the input """
-    chain = prompt | llm | output_parser
-    
-    return chain.invoke(topic)
-
-
-def start():
-    instructions = (
-        "Type your topic and press ENTER. Type 'x' to go back to the MAIN menu.\n"
-    )
-    print(Fore.BLUE + "\n\x1B[3m" + instructions + "\x1B[0m" + Fore.RESET)
-
-    print("MENU")
-    print("====")
-    print("[1]- Enter a Topic")
-    print("[2]- Exit")
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        ask()
-    elif choice == "2":
-        print("Goodbye!")
-        exit()
-    else:
-        print("Invalid choice")
-        start()
-
-
-def ask():
-    while True:
-        user_input = input("Q: ")
-        # Exit
-        if user_input == "x":
-            start()
-        else:
-            response = generate(user_input)
-            print(Fore.BLUE + f"A: " + response + Fore.RESET)
-            print(Fore.WHITE + "\n-------------------------------------------------")
-
-
-if __name__ == "__main__":
-    start()
