542dbf09aee95a153961a2f7962f9a33ff7300a5
Suraj Bhatt <surajbhatt54321@gmail.com>
2024-12-14 21:32:20 +0530

    query as a retriever

--- 4.py
+++ 4.py
@@ -8,16 +8,19 @@ from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.vectorstores import FAISS
+from langchain_core.runnables import RunnablePassthrough
 
 
 load_dotenv()
+
+model = OpenAI()
 
 template = """Answer the question based only on the following context:
 {context}
 
 Question: {question}
 """
-prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}?Only give joke.")
+prompt = ChatPromptTemplate.from_template(template)
 
 llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 output_parser = StrOutputParser()
@@ -27,5 +30,23 @@ vectorstore = FAISS.from_texts(["harrison worked on kensho"], embedding=OpenAIEm
 
 # querying the vector store
 query = "Where did Harrison work?"
-docs = vectorstore.similarity_search(query, top_k=1)
-print(docs[0].page_content)
+# docs = vectorstore.similarity_search(query, top_k=1)
+# print(docs[0].page_content)
+
+# querying as retriever
+retriever = vectorstore.as_retriever()
+# docs = retriever.invoke(query, top_k=1)
+# print(docs[0].page_content)
+
+retriever_chain = (
+    {
+        "context": retriever,
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+response = retriever_chain.invoke(query)
+print(response)
